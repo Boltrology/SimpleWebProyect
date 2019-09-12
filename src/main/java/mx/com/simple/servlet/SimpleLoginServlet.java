b58d5fb3c7312758251cebb9acf0351aefb8c000
Rodrigo Garcia <contacto@rodrigogc.net>
2019-09-12 11:03:51 -0500

    Se simplfico el login, para que el proyecto fuera mas facil de explicar.

---
+++ src/main/java/mx/com/simple/servlet/SimpleLoginServlet.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.simple.servlet;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author RGC
+ */
+@WebServlet(name = "SimpleLoginServlet", urlPatterns = {"/login.do"})
+public class SimpleLoginServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String user = request.getParameter("email");
+        String password = request.getParameter("password");
+        RequestDispatcher rd = request.getRequestDispatcher("response.jsp");
+
+        if (user.equals("rodrigo@garcia.com") && password.equals("holamundo")) {
+            request.setAttribute("response", "Bienvenido, Rodrigo!!");
+        } else if (user.equals("miguel@angel.com") && password.equals("holamundo")) {
+            request.setAttribute("response", "Bienvenido, Rodrigo!!");
+        } else if (user.equals("karen@abigail.com") && password.equals("holamundo")) {
+            request.setAttribute("response", "Bienvenido, Miguel Angel!!");
+        } else {
+            request.setAttribute("response", "No se pudo autenticar el usuario");
+        }
+        
+        rd.forward(request, response);
+    }
+
+}
--- src/main/webapp/response.jsp
+++ src/main/webapp/response.jsp
@@ -54,22 +54,7 @@
         <br>
 
         <div class="card mx-auto col-sm-6">
-            <form action="login.do" method="POST">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Correo</label>
-                    <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese un correo electronico">
-
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Clave</label>
-                    <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Clave">
-                </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">¡Recuerdame!</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>
+            <h1> ${response} </h1>
         </div>
 
     </body>
